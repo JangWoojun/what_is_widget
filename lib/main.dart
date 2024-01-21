ca131ed65a6eced8b8994edc56d78a797d420cf0
JangWoojun <woojun0107@naver.com>
2024-01-22 07:18:34 +0900

    design: body 화면 디자인

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,27 @@
 import 'package:flutter/material.dart';
 
-oid main() {
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+        body: TextWidget()),
+  ));
+}
 
+class TextWidget extends StatelessWidget {
+  const TextWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Center(
+        child: Text(
+          "Hello Flutter",
+          style: TextStyle(
+            fontSize: 40,
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
 }
